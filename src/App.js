b3e4a234fdf7399e4901cc92fa7f10680932eb45
jesus <Jesusfernandorios93@gmail.com>
2020-10-06 14:49:54 -0700

    finished created header

--- src/App.js
+++ src/App.js
@@ -18,6 +18,50 @@ function App() {
           <Route path="/">
             <Header/>
             <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
+            <h1>Home Page</h1>
           </Route>
         </Switch>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,7 +43,7 @@ function Header() {
                 <Link to="/checkout" className="header__link">
                     <div className="header__optionBasket">
                         <ShoppingBasketIcon/>
-                        <span className="header__optionLineTwo">0</span>
+                        <span className="header__optionLineTwo header__basketCount">0</span>
                     </div>
                 </Link>
             </div>
